738233558e14fabdd2c66f07a0afdf348e255681
dm1triykr <dimonk601@gmail.com>
2023-03-01 01:13:34 +0200

    added radio button's functional

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
+
 $(document).foundation();
 
 $(function() {
 var storageSlider = $('#storageSlider');
 var transferSlider = $('#transferSlider');
 var progressBars = $('.progress');
+var optionBtns = $('.providers__radio-button');
+
+optionBtns.on('change', function(e) {
+    var relatedProgress = $(this).data('related-progress'),
+        optionValue = $(this).data('value'),
+        optionName = $(this).attr('name');
+        console.log(optionName);
+
+    if ($('#' + relatedProgress).length) {
+        if(optionName == 'storage-bunny') {
+            var optionReplacingName = optionName.replace('-bunny', "");
+            $('#' + relatedProgress).data(optionReplacingName + '-modifier', optionValue);
+            updateOutputsAndProgressBars();
+        } else {
+            var optionReplacingName = optionName.replace('-scaleway', "");
+            $('#' + relatedProgress).data(optionReplacingName + '-modifier', optionValue);
+            updateOutputsAndProgressBars();
+        }
+    }
+});
 
 // Listen for the changed.zf.slider event on both sliders
 storageSlider.on('changed.zf.slider', function(e, handle) {
@@ -17,17 +38,13 @@ transferSlider.on('changed.zf.slider', function(e, handle) {
 // Update the outputs and progress bars based on the current slider values
 function updateOutputsAndProgressBars() {
     var storageValue = storageSlider.find('.slider-handle').attr('aria-valuenow');
-    console.log(storageValue);
     var transferValue = transferSlider.find('.slider-handle').attr('aria-valuenow');
-    console.log(transferValue);
     var maxValue = 100;
 
     // Update the progress bars based on the percentage of total storage and transfer values
     progressBars.each(function(index, progressBar) {
         var storageModifier = $(progressBar).data('storage-modifier');
-        //console.log(storageModifier);
         var transferModifier = $(progressBar).data('transfer-modifier');
-        //console.log(transferModifier);
         var totalValue = storageValue * storageModifier + transferValue * transferModifier;
 
         if (totalValue > maxValue) {
@@ -41,4 +58,3 @@ function updateOutputsAndProgressBars() {
     });
 }
 });
-    
